2f88d2e590890f297cab59865448e8eb8ceeedbf
SeregaGG <48214766+SeregaGG@users.noreply.github.com>
2019-10-23 23:31:10 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -33,6 +33,27 @@ function customBind (func, context, ...args) {
  * sum :: void -> Number
  */
 function sum (x) {
+    if (x === undefined) {
+        return 0;
+    }
+
+    let sumAllElements = x;
+
+    function sumNext (nextEltmtnt) {
+        if (nextEltmtnt === undefined) {
+            return sumNext.Number();
+        }
+        
+        sumAllElements += nextEltmtnt;
+
+        return sumNext;
+    }
+
+    sumNext.Number = function () {
+        return sumAllElements;
+    };
+
+    return sumNext;
 }
 module.exports = {
     customBind,
