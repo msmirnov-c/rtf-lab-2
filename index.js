b2e2db952071c9b61ce5d74d505fd9f89e357737
SeregaGG <48214766+SeregaGG@users.noreply.github.com>
2019-10-24 00:06:32 +0500

    Попробовал убрать комментарии

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/**
- * Создайте собственную реализацию функции bind
- * @param {Function} func передаваемая функция
- * @param {any} context контекст
- * @param {Array<any>} args массив аргументов
- * @return {Function} функция с нужным контекстом
- */
 function customBind (func, context, ...args) {
     const arrayAllArgs = [];
 
@@ -23,15 +16,6 @@ function customBind (func, context, ...args) {
     return f;
 }
 
-/* ============================================= */
-
-/**
- * Напишите функцию sum, вычисляющую суммы подобным образом:
- * sum(1)(2)( ) // 3
- * sum(1)(2)(3)( ) // 6
- * sum :: Number -> sum
- * sum :: void -> Number
- */
 function sum (x) {
     if (x === undefined) {
         return 0;
@@ -45,7 +29,7 @@ function sum (x) {
         }
         
         sumAllElements += nextEltmtnt;
-        
+
         return sumNext;
     }
 
