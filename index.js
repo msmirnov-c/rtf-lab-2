4a4ea14377dded3d5bd17e3faa0fa1e140a4a475
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-10-25 00:39:09 +0500

    Синтаксис(точка с запятой)

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
  * @return {Function} функция с нужным контекстом
  */
 function customBind (func, context, ...args) {
-    return function(...newArgs) {
+    return function (...newArgs) {
         return func.apply(context, args.push(newArgs));
-    }
+    };
 }
 
 /* ============================================= */
