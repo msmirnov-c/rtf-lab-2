8ebda9e7ca90fd92ae6b4633a3975ba7bb2d21dd
SerGeKon <lordbos@mail.ru>
2019-10-24 15:43:17 +0300

    update sum

--- index.js
+++ index.js
@@ -6,27 +6,26 @@
  * @return {Function} функция с нужным контекстом
  */
 function customBind (func, context, ...args) {
-    const functionArgs = [].slice.call(arguments);
-
-    return func.apply(context, args.concat(functionArgs));
+    return func.apply(context, args.concat([].slice.call(arguments)));
 }
 
 function sum (x) {
-    if (x !== undefined) {
-        let sum = x;
-
-        return function sumY (y) {
-            if (y !== undefined) {
-                sum += y;
-
-                return sumY;
-            } else {
-                return sum;
-            }
-        };
+    if (x === undefined) {
+        return 0;
     }
 
-    return 0;
+    let funcSum = x;
+    function sumY (y) {
+        if (y !== undefined) {
+            funcSum += y;
+
+            return sumY;
+        } else {
+            return funcSum;
+        }
+    };
+
+    return sumY;
 }
 
 module.exports = {
