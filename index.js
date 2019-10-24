6360449e9d4004967ac03dd52c3569a46d4642f0
OvchinnikovJMA <47637806+OvchinnikovJMA@users.noreply.github.com>
2019-10-24 22:01:07 +0500

    синтаксические ошибки
    исправляем

--- index.js
+++ index.js
@@ -6,30 +6,23 @@
  * @return {Function} функция с нужным контекстом
  */
 function customBind (func, context, ...args) {
-    return function f(){
+    return function f () {
         return func.apply(context, args.slice.call(arguments));
-    }
-    
+    }; 
 }
 
 /* ============================================= */
 
-/**
- * Напишите функцию sum, вычисляющую суммы подобным образом:
- * sum(1)(2)( ) // 3
- * sum(1)(2)(3)( ) // 6
- * sum :: Number -> sum
- * sum :: void -> Number
- */
-
-function sum(x = 0) {
+function sum (x = 0) {
     var l = x;
-    function nextSum (t = 0) {
+
+    function first (t = 0) {
        l += t;
-       return nextSum;
+       return first;
     }
-    nextSum.valueOf = () => { return l };
-    return nextSum;
+
+    first.valueOf = () => { return l };
+    return first;
 }
 
 module.exports = {
