c2d0338833680add385fda5ba99e0813cdc3a83a
Selyankin Mihail <reckleyoff@gmail.com>
2019-10-25 23:25:36 +0500

    Fix

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
  * @return {Function} функция с нужным контекстом
  */
 function customBind (func, context, ...args) {
-    return function (...bindedArgs) {
-        return func.apply(context, args.concat(bindedArgs));
+    return (...bindedArgs) => {
+        return func.apply(context, [].concat(args).concat(bindedArgs));
     };
 }
 
