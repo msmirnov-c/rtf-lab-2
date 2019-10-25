b08c9d9d538652141cb9c0d5c7479081f0383f97
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-10-25 09:34:59 +0500

    Исправил в условии == на ===

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function customBind (func, context, ...args) {
 function sum (x) {
     let tempSum = x;
     function sumNextBrackets (nextArg) {
-        if (nextArg == undefined) {
+        if (nextArg === undefined) {
             return tempSum;
         }
 
@@ -32,7 +32,7 @@ function sum (x) {
         return sumNextBrackets;
     }
 
-    if (tempSum == undefined) {
+    if (tempSum === undefined) {
         return 0;
     }
 
