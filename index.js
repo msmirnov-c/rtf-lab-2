8f899976fb2b2c94059ccbcfaf403316c915ee00
Sokolov Max <48237187+Maxerdan@users.noreply.github.com>
2019-10-25 16:52:16 +0500

    Убрал slice в прошлом коммите, убрал лишние комментарии

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-/**
- * Создайте собственную реализацию функции bind
- * @param {Function} func передаваемая функция
- * @param {any} context контекст
- * @param {Array<any>} args массив аргументов
- * @return {Function} функция с нужным контекстом
- */
 function customBind (func, context, ...args) {
     return function (...newArgs) {
         return func.apply(context, args.concat(newArgs));
     };
 }
 
-/* ============================================= */
-
-/**
- * Напишите функцию sum, вычисляющую суммы подобным образом:
- * sum(1)(2)( ) // 3
- * sum(1)(2)(3)( ) // 6
- * sum :: Number -> sum
- * sum :: void -> Number
- */
 function sum (x) {
     let tempSum = x;
     function sumNextBrackets (nextArg) {
