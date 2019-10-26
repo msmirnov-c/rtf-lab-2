4a4769302ab10a55f23cb49dc4dca4cb823d6b86
RomanGrishin <56441172+RomanGrishin@users.noreply.github.com>
2019-10-26 22:37:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 /**
- * Создайте собственную реализацию функции bind
- * @param {Function} func передаваемая функция
- * @param {any} context контекст
- * @param {Array<any>} args массив аргументов
- * @return {Function} функция с нужным контекстом
- */
-function customBind (func, context, ...args) {
-    // code
-}
+* Создайте собственную реализацию функции bind
+* @param {Function} func передаваемая функция
+* @param {any} context контекст
+* @param {Array<any>} args массив аргументов
+* @return {Function} функция с нужным контекстом
+*/
+function customBind(func, context, ...args) {
 
+    return func.apply(context, args.concat(newArgs));
+}
 /* ============================================= */
 
 /**
- * Напишите функцию sum, вычисляющую суммы подобным образом:
- * sum(1)(2)( ) // 3
- * sum(1)(2)(3)( ) // 6
- * sum :: Number -> sum
- * sum :: void -> Number
- */
-function sum (x) {
-    // code
+* Напишите функцию sum, вычисляющую суммы подобным образом:
+* sum(1)(2)( ) // 3
+* sum(1)(2)(3)( ) // 6
+* sum :: Number -> sum
+* sum :: void -> Number
+*/
+function sum(x) {
+    if (x === undefined) {
+
+        return 0;
+
+    } else {
+        let value = x;
+        function Sum(x) {
+            if (x !== undefined) {
+                value += x;
+
+                return Sum;
+            }
+
+            return value;
+        }
+        return Sum;
+    }
+
 }
 
 module.exports = {
