009450bd2f5d358f753b3fcba5a8469a84ec31b4
Никита Паренюк <48210690+nekitlnp@users.noreply.github.com>
2019-10-28 00:07:22 +0500

    Лаб 2

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-/**
- * Создайте собственную реализацию функции bind
- * @param {Function} func передаваемая функция
- * @param {any} context контекст
- * @param {Array<any>} args массив аргументов
- * @return {Function} функция с нужным контекстом
- */
 function customBind (func, context, ...args) {
-    // code
+    return function () {
+        return func.apply(context, args);
+    };
 }
 
-/* ============================================= */
+function sum (x = 0) {
+    var count = x;
 
-/**
- * Напишите функцию sum, вычисляющую суммы подобным образом:
- * sum(1)(2)( ) // 3
- * sum(1)(2)(3)( ) // 6
- * sum :: Number -> sum
- * sum :: void -> Number
- */
-function sum (x) {
-    // code
+    return function by (y) {
+        if (y === undefined) {
+            return count;
+        } else {
+            count += y;
+
+            return by;
+        }
+    };
 }
 
 module.exports = {
