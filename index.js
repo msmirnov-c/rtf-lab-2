e40bc41987f5ae11427bc035c0b295a60f4c6eb9
Komodood <36847935+Komodood@users.noreply.github.com>
2019-10-28 15:31:17 +0500

    Update index.js
    Оба метода, Bind не проверял

--- index.js
+++ index.js
@@ -6,7 +6,10 @@
  * @return {Function} функция с нужным контекстом
  */
 function customBind (func, context, ...args) {
-    // code
+    function customBind (func, context, ...args) {
+        return func.apply(context, args.slice.call(arguments));
+        }
+        //слава https://learn.javascript.ru/call-apply
 }
 
 /* ============================================= */
@@ -18,9 +21,22 @@ function customBind (func, context, ...args) {
  * sum :: Number -> sum
  * sum :: void -> Number
  */
-function sum (x) {
-    // code
+function sum (x)
+{
+    let First = x;
+    if(First===undefined){
+        return 0;
+    }
+    function BoyNextDoor(Next){
+    if(Next===undefined){
+        return First;
+    }
+    First+= Next;
+    return BoyNextDoor;
+    }
+return BoyNextDoor;
 }
+
 
 module.exports = {
     customBind,
