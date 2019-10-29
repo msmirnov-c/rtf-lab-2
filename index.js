52402ea6769784dd40a97400a1490c7ee99f0cb4
Elena Fedulova <48327813+needMoreBlood@users.noreply.github.com>
2019-10-29 21:01:08 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
 /**
+
  * Создайте собственную реализацию функции bind
+
  * @param {Function} func передаваемая функция
+
  * @param {any} context контекст
+
  * @param {Array<any>} args массив аргументов
+
  * @return {Function} функция с нужным контекстом
+
  */
+
 function customBind (func, context, ...args) {
-    return function (arguments) {
+
+    return function (...arguments) {
+        
         return func.apply(context, args.concat(arguments));
-    };
+    }
+
 }
+
+
 
 /* ============================================= */
 
+
+
 /**
+
  * Напишите функцию sum, вычисляющую суммы подобным образом:
+
  * sum(1)(2)( ) // 3
+
  * sum(1)(2)(3)( ) // 6
+
  * sum :: Number -> sum
+
  * sum :: void -> Number
+
  */
-let result = 0;
+
 function sum (x) {
+    
     if (x === undefined) {
         return 0;
     }
-   return (newSum) => (newSum === undefined) ? x : sum (x += newSum);
+
+    return (...arguments) => (x === undefined) ? x : sum (x += arguments);
 }
 
+
+
 module.exports = {
+
     customBind,
+
     sum
+
 };
