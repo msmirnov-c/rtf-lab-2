5c144ab3623cfe04f0ce4367d435af7c6be7edf7
D.Kuvashov <d.kuvashov@gmail.com>
2019-10-29 15:09:34 +0500

    second task

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * @param {Array<any>} args массив аргументов
  * @return {Function} функция с нужным контекстом
  */
-function customBind(func, context, ...args) {
+function customBind (func, context, ...args) {
     // code
     // test commit
 }
@@ -21,7 +21,7 @@ function customBind(func, context, ...args) {
  */
 let n = 0;
 
-function sum(x) {
+function sum (x) {
     // code
     if (typeof (x) !== 'number') {
         if (n === 0) {
