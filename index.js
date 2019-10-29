b571e030f420b1abbdb4cdd61e2f298009079f46
Daniil Popov <p.danya@mail.ru>
2019-10-29 14:16:19 +0500

    fix_code_style

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  */
 function customBind (func, context, ...args) {
     return function (...newArgs) {
-       return func.apply(context, args.concat(newArgs));
+        return func.apply(context, args.concat(newArgs));
     };
 }
 
