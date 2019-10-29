3091f6082412dd94c9c0bcae008e23978da205ab
M1M0G <M1M0G@github.com>
2019-10-29 18:14:43 +0500

    Вот теперь точно работает

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  * @return {Function} функция с нужным контекстом
  */
 function customBind (func, context, ...args) {
-    return (...newargs) => func.apply(context, ...args.concat(...newargs));
+    return (...newargs) => func.apply(context, args.concat(...newargs));
 }
 
 function sum (x) {
