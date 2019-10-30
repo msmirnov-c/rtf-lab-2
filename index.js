b0d80762f10faba19e9d9c76466b012499eb2cff
ToottiFroottiTitoto <56549951+ToottiFroottiTitoto@users.noreply.github.com>
2019-10-30 23:25:09 +0500

    6

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  */
 function customBind (func, context, ...args) {
     return function () {
-        return func.apply(context, b.concat(...args));
+        return func.apply(context, ...args.concat(...args));
     };
 };
 
