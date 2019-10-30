55370200c3b73c258fefdc7a64b9f6180b1759ec
RomanGrishin <56441172+RomanGrishin@users.noreply.github.com>
2019-10-30 19:00:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 * @param {Array<any>} args массив аргументов
 * @return {Function} функция с нужным контекстом
 */
-function customBind(func, context, ...args) {
+function customBind (func, context, ...args) {
     return function (...arg) {
         return func.apply(context, args.concat(arg));
     };
@@ -19,13 +19,14 @@ function customBind(func, context, ...args) {
 * sum :: Number -> sum
 * sum :: void -> Number
 */
-function sum(x) {
+function sum (x) {
     if (x === undefined) return 0;
 
-    return function calcSum(value) {
+    return function calcSum (value) {
         switch (value) {
         case (!undefined):
             x = x + value;
+
             return calcSum;
 
         case (undefined):
