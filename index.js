d8905d1151f6725edf9383a168df334dc5734253
Maria <48256725+mashaartemeva@users.noreply.github.com>
2019-10-30 13:20:16 +0500

    Update index.js
    пытаюсь решить проблемы кодстайла

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ function customBind (func, context, ...args) {
  * sum :: void -> Number
  */
 function sum (x) {
-    if ( typeof(x) === 'undefined') return 0;
+    if (typeof x === 'undefined') return 0;
     
     let result = x;
-    function getSum (nextX) {
-        
-        if ( typeof(nextX) === 'undefined') return result;
+    
+    function getSum(nextX) {
+        if (typeof nextX === 'undefined') return result;
         
         result += nextX;
         
