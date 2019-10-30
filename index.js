47a338a3d3e8bff6771464eff778e45549cc18ba
ToottiFroottiTitoto <56549951+ToottiFroottiTitoto@users.noreply.github.com>
2019-10-30 23:00:39 +0500

    5

--- index.js
+++ index.js
@@ -6,13 +6,12 @@
  * @return {Function} функция с нужным контекстом
  */
 function customBind (func, context, ...args) {
-    const a = function (func, context) {
-        const b = [].slice.call(...args, 2);
+    const b = [].slice.call(...args, 2);
+    
+    return function () {
+        var c = [].slice.call(...args);
 
-        return function () {
-            var c = [].slice.call(...args);
-
-            return func.apply(context, b.concat(c));
+        return func.apply(context, b.concat(c));
         };
     };
 }
