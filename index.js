681675b1346dee0031438197c856a83ba8afcb28
Dmitriy Baygulov <baygulov.d@yandex.ru>
2019-10-30 13:26:04 +0500

    codestyle (try2)

--- index.js
+++ index.js
@@ -5,28 +5,29 @@
  * @param {Array<any>} args массив аргументов
  * @return {Function} функция с нужным контекстом
  */
-function customBind(func, context, ...args) {
-  return function(...newArgs) {
-    return func.apply(context, args.concat(newArgs));
-  };
+function customBind (func, context, ...args) {
+    return function (...newArgs) {
+        return func.apply(context, args.concat(newArgs));
+    };
 }
 
 /* ============================================= */
 
-function sum(x) {
-  if (x === undefined) {
-    return 0;
-  }
-
-  return function(num) {
-    if (num !== undefined) {
-      return sum(x + num);
+function sum (x) {
+    if (x === undefined) {
+        return 0;
     }
-    return x;
-  };
+
+    return function (num) {
+        if (num !== undefined) {
+            return sum(x + num);
+        }
+
+        return x;
+    };
 }
 
 module.exports = {
-  customBind,
-  sum
+    customBind,
+    sum
 };
